070eb2ab30eec1a3387c8a7b0c19652dca0cacaa
UsaSai99charanteja <saicharantejausac@gmail.com>
2023-10-25 11:01:58 -0500

    updating the backed infra for tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Charan-demo-tfstate-bucket"
+    bucket = "charan-demo-tfstate-bucket"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
